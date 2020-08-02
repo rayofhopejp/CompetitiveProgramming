e6e970276d6064f6681fe8d48b3c8131b55d644c
NaomiatLibrary <ray.oshikawa@gmail.com>
2020-08-03 00:06:14 +0900

    test.shを変更し、ファイル名の変更に対応しました。

--- C++Workspace/test.sh
+++ C++Workspace/test.sh
@@ -2,7 +2,7 @@
 problemname=$1
 oj dl "https://${problemname:0:6}.contest.atcoder.jp/tasks/${problemname:0:8}"
 #oj dl "https://atcoder.jp/contests/panasonic2020/tasks/panasonic2020_${problemname:0:1}"
-g++ -Wall -std=c++14 ./main.cpp
+g++ -Wall -std=c++14 ./solved/${problemname}.cpp
 oj test
 rm -f a.out
 rm -rf test 
